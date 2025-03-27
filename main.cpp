ece4553347e4c6442766893a76e08d52de4d0c83
sean <Seanscanlon0222@gmail.com>
2025-03-26 20:26:25 -0400

    added inventory class and fixed character class, again

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 
 class character {
     public:
@@ -19,6 +22,10 @@ class character {
         public:
             // string to store location, might change later
             std::string room;
+            // added x and y coordinates for easy map making if i figure out how to use a gui eventually...
+            // felt cute might delete later...                                             lol (;
+            int xCoord;
+            int yCoord;
         
             void changeLocation(std::string x) {
                 if (x == "bedroom" || x == "bathroom" || x == "kitchen") {
@@ -26,17 +33,40 @@ class character {
                 }
         }
     }; 
+
+    class Inventory {
+            std::string currentItem;
+    
+        public:
+            void setItem(const std::string& item) {
+                currentItem = item;
+            }
+
+        void clearItem() {
+            currentItem.clear();
+        }
+        
+        void checkItem() {
+            if (currentItem.empty()) {
+                std::cout << "you are not holding anything\n";
+            } else {
+                std::cout << "you are holding: " << currentItem << "\n";
+            }
+            
+        }
+    };
+    //! initialize all classes locally to character class so each character can have its own location, inventory etc
+    Inventory inventory;
     Location location;
 };
 
 
 
 int main() {
+    // test
    character sean;
-   //test
-    sean.location.changeLocation("bathroom");
-    std::cout << sean.location.room << "\n";
-    /*
-    this is a test for git
-    */
+   sean.inventory.setItem("dog");
+   sean.inventory.clearItem();
+   sean.inventory.setItem("can");
+   sean.inventory.checkItem();
 }
